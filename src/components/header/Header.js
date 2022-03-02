ea4fe85554e5feae2e8054667b7b15fd7b5e58a1
Anguswaffle <90980870+Anguswaffle@users.noreply.github.com>
2022-03-01 20:31:33 -0800

    adding pic

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -1,10 +1,13 @@
+import './header.css'
 import Navbar from "../navbar/Navbar"
+import ProfilePic from '../../assets/portfolio-pic.png'
 
 export default function Header() {
 
   return (
-    <div>
+    <div className="flex-column">
       <Navbar />
+      <img alt={'Aidan playing the ukulele while smiling'} src={ProfilePic}/>
     </div>
   )
 }
--- src/components/navbar/Navbar.js
+++ src/components/navbar/Navbar.js
@@ -5,13 +5,23 @@ import { SmileOutlined, ProjectOutlined, PhoneOutlined, FileTextOutlined } from 
 export default function Navbar() {
 
   return (
-    // <div className="display-flex justify-flex-end">
-      <nav className="display-flex justify-space-around">
-        <NavLink to='/'><SmileOutlined />About Me</NavLink>
-        <NavLink to='/projects'><ProjectOutlined /> Projects</NavLink>
-        <NavLink to='/contact'><PhoneOutlined /> Contact</NavLink>
-        <NavLink to='/resume'><FileTextOutlined /> Resume</NavLink>
-      </nav>
-    // </div>
+    <nav className="display-flex justify-space-around">
+      <NavLink to='/'>
+        <SmileOutlined />
+        About Me
+      </NavLink>
+      <NavLink to='/projects'>
+        <ProjectOutlined />
+        Projects
+      </NavLink>
+      <NavLink to='/contact'>
+        <PhoneOutlined />
+        Contact
+      </NavLink>
+      <NavLink to='/resume'>
+        <FileTextOutlined />
+        Resume
+      </NavLink>
+    </nav>
   );
 }
