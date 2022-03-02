2c937ec0d9f237608390715570945be0c46899a1
Anguswaffle <90980870+Anguswaffle@users.noreply.github.com>
2022-03-02 00:58:18 -0800

    added icon hover toggle

--- src/components/navbar/Navbar.js
+++ src/components/navbar/Navbar.js
@@ -1,5 +1,6 @@
+import './navbar.css'
 import { NavLink } from "react-router-dom";
-import { SmileOutlined, ProjectOutlined, PhoneOutlined, FileTextOutlined } from "@ant-design/icons/lib/icons";
+import { SmileOutlined, SmileFilled, ProjectOutlined, ProjectFilled, PhoneOutlined, PhoneFilled, FileTextOutlined, FileTextFilled } from "@ant-design/icons/lib/icons";
 
 
 export default function Navbar() {
@@ -7,20 +8,32 @@ export default function Navbar() {
   return (
     <nav className="display-flex justify-space-around">
       <NavLink to='/'>
-        <SmileOutlined />
-        About Me
+        <span className='about-me'>
+          <SmileOutlined className='default-smile' />
+          <SmileFilled className='hover-smile' />
+          About Me
+        </span>
       </NavLink>
       <NavLink to='/projects'>
-        <ProjectOutlined />
-        Projects
+        <span className='project'>
+          <ProjectOutlined className='default-project' />
+          <ProjectFilled className='hover-project' />
+          Projects
+        </span>
       </NavLink>
       <NavLink to='/contact'>
-        <PhoneOutlined />
-        Contact
+        <span className='contact'>
+          <PhoneOutlined className='default-contact' />
+          <PhoneFilled className='hover-contact' />
+          Contact
+        </span>
       </NavLink>
       <NavLink to='/resume'>
-        <FileTextOutlined />
-        Resume
+        <span className='resume'>
+          <FileTextOutlined className='default-resume' />
+          <FileTextFilled className='hover-resume' />
+          Resume
+        </span>
       </NavLink>
     </nav>
   );
