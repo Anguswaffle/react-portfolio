23677bcb41482fabd727aede7e48c26ffb1f03e6
Anguswaffle <90980870+Anguswaffle@users.noreply.github.com>
2022-03-02 10:27:35 -0800

    added home

--- src/components/home/Home.js
+++ src/components/home/Home.js
@@ -1,8 +1,12 @@
 export default function Home() {
 
-  return(
-    <div>
-      
-    </div>
+  return (
+    // <div className="home">
+    <article className="text-center">
+      <p>
+        Hi! I'm Aidan Albano-Bachtell. I'm a fullstack engineer in Palm Springs, CA. In the past, I taught elementary music.
+      </p>
+    </article>
+    // </div>
   )
 }
