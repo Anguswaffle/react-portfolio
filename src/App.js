a328f1f7201b7e47f16e86c532e76b37380af2d2
Anguswaffle <90980870+Anguswaffle@users.noreply.github.com>
2022-03-01 22:33:06 -0800

    removed navbar from app.js

--- src/App.js
+++ src/App.js
@@ -4,7 +4,6 @@ import Contact from './components/contact/Contact';
 import Footer from './components/footer/Footer';
 import Header from './components/header/Header';
 import Home from './components/home/Home';
-// import Navbar from './components/navbar/Navbar';
 import Project from './components/project/Project';
 import { Routes, Route } from 'react-router-dom';
 
