084f343b0e8df6f0fbc026ba0dac5b8cb6ef42e1
Anguswaffle <90980870+Anguswaffle@users.noreply.github.com>
2022-03-02 11:50:54 -0800

    adjusted media query settings

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -1,13 +1,12 @@
 import './header.css'
 import Navbar from "../navbar/Navbar"
-import ProfilePic from '../../assets/portfolio-pic.png'
 
 export default function Header() {
 
   return (
     <div className="flex-column">
       <Navbar />
-      <img alt={'Aidan playing the ukulele while smiling'} src={ProfilePic}/>
+
     </div>
   )
 }
--- src/components/home/Home.js
+++ src/components/home/Home.js
@@ -1,12 +1,16 @@
+import './home.css'
+import ProfilePic from '../../assets/portfolio-pic.png'
+
 export default function Home() {
 
   return (
-    // <div className="home">
+    <div className="flex-column align-center">
+            <img className="col-sm-6 col-md-4 col-2" alt={'Aidan playing the ukulele while smiling'} src={ProfilePic}/>
     <article className="text-center">
       <p>
         Hi! I'm Aidan Albano-Bachtell. I'm a fullstack engineer in Palm Springs, CA. In the past, I taught elementary music.
       </p>
     </article>
-    // </div>
+    </div>
   )
 }
